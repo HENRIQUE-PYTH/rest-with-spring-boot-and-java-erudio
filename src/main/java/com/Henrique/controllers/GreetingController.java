ca354c06fbe96814cdd469f77e1f213fa920554f
Henriique_ <enzohenriquealves42@gmail.com>
2025-07-21 18:55:34 -0300

    Alteração na anotação da linha 20, e nos parametros da anotação da linha 13, mudado tambem o tipo na linha 21,e o parametro na linha 24

--- src/main/java/com/Henrique/controllers/GreetingController.java
+++ src/main/java/com/Henrique/controllers/GreetingController.java
@@ -1,5 +1,7 @@
 package com.Henrique.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,19 +10,18 @@ import com.Henrique.model.Greeting;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RequestMapping("/api/v1/greeting")
 public class GreetingController {
 
     private static final String template = "Hello, %s! ";
     private final AtomicLong counter = new AtomicLong();
 
     // http://localhost:8080/greeting?nome=henrique
-    @RequestMapping("/greeting")
-    public Greeting greeting(
+    @GetMapping
+    public ResponseEntity<Greeting> greeting(
             @RequestParam(value = "nome", defaultValue = "Henrique")
             String name){
-        return new Greeting (counter.incrementAndGet(), String.format(template, name));
-
-
+        return ResponseEntity.ok(new Greeting (counter.incrementAndGet(), String.format(template, name)));
     }
 
 }
