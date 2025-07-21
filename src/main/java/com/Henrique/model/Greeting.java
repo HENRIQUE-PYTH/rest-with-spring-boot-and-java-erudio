d8a0314372bf82e7427182909649922251a97387
Henriique_ <enzohenriquealves42@gmail.com>
2025-07-21 14:55:40 -0300

    Primeiro commit do projeto Spring Boot

---
+++ src/main/java/com/Henrique/model/Greeting.java
@@ -0,0 +1,3 @@
+package com.Henrique.model;
+
+public record Greeting(long id, String content) {}
---
+++ src/test/java/com/Henrique/StartupTests.java
@@ -0,0 +1,13 @@
+package com.Henrique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
